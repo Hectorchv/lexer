03917b97c86711bc51c36d98c2d10be1b6c50068
Hectorchv <hectorchv045@gmail.com>
2023-10-17 19:18:40 -0600

    Segundo avance

---
+++ lista.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+
+#include"lista.h"
+
+add_t_numerica(t_numerica *tabla, float valor)
+{
+    t_numerica *tmp = malloc(sizeof(t_numerica));
+
+    assert(tmp != NULL);
+
+    
+}
+
+add_t_cadena(t_cadena *tabla, char *valor);
+add_t_ident(t_ident *tabla, char *nombre, int tipo);
--- lista.h
+++ lista.h
@@ -1,35 +1,46 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-typedef struct numerica
+
+//Tabla de constantes numericas
+typedef struct num
 {
     unsigned pos;
     float valor;
-    numerica *next;
-};
+    struct num *next;
+} t_numerica;
 
-typedef struct cadena
+//Tabla de cadenas
+typedef struct cad
 {
     unsigned pos;
+    char *valor;
+    struct cad *next;
+} t_cadena;
 
-};
-
-typedef struct ident
+//Tabla de identificadores
+typedef struct id
 {
     unsigned pos;
     char *nombre;
     int tipo;
-    ident *next;
-};
+    struct id *next;
+} t_ident;
 
-typedef struct token
+//Tabla de tokens
+typedef struct tok
 {
     int clase;
     int valor;
-    token *next;
-};
+    struct tok *next;
+} t_token;
+
 
 int addtoken(int clase, int valor);
+
+int add_t_numerica(t_numerica *tabla, float valor);
+int add_t_cadena(t_cadena *tabla, char *valor);
+int add_t_ident(t_ident *tabla, char *nombre, int tipo);
 
 
 
